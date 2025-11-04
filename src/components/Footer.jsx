fc7c90a00646c1e7c898670f9163da5d296d4199
MohamedFawzi51 <fawzimohamed4151@gmail.com>
2025-11-04 06:59:08 +0200

    Initial portfolio commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-function Footer(){
-    return(
-        <div class="text-center p-6 bg-black footer">
-        <span class="text-white">&copy; 2023 Copyright - </span>
-        <a class="link-underline text-white font-bold" href="https://rahulkarda.netlify.app">Rahul Karda</a>
-      </div>
-    )
+function Footer() {
+	return (
+		<div class="text-center p-6 bg-black footer">
+			<span class="text-white">&copy; 2023 Copyright - </span>
+			<a
+				class="link-underline text-white font-bold"
+				href="https://rahulkarda.netlify.app">
+				Mohamed Fawzi
+			</a>
+		</div>
+	);
 }
 
-
-export default Footer
+export default Footer;
